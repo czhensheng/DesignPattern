a5886f5073a35dd634d61a7afe546a5b83eff28a
czhensheng <358430429@qq.com>
2018-06-03 18:20:21 +0800

    commit Adapter Pattern Demo

---
+++ AdapterPattern/Charger/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Charger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("手机:");
+            ITarget t=new Adapter();
+            t.GetPower();
+            System.Console.WriteLine("-----------------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+
+    ///类似配器结构
+    public interface ITarget
+    {
+        void GetPower();
+    }
+
+    public abstract class Power{
+        public void GetPower220(){
+            System.Console.WriteLine("从电源中得到220V电压");
+        }
+    }
+
+    public class Adapter:Power,ITarget{
+        public void GetPower(){
+            this.GetPower220();
+            System.Console.WriteLine("得到手机的充电电压");
+        }
+    }
+
+}
+
---
+++ AdapterPattern/classCharger/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace classCharger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITarget t=new Adapter(new Power());
+            t.GetPower();
+            Console.ReadLine();
+        }
+    }
+
+
+    public interface ITarget
+    {
+        void GetPower();
+    }
+
+    public class Power{
+        public void GetPower220(){
+            System.Console.WriteLine("从电源那里获取220V电压 ");
+        }
+    }
+
+    public class Adapter:ITarget{
+        public Power _power;
+        public Adapter(Power power)
+        {
+            _power=power;
+        }
+        public void GetPower(){
+            _power.GetPower220();
+            System.Console.WriteLine("得到手机充电电压");
+        }
+    }
+
+}
+
